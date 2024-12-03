f9dcf70dfc2120c60feb4dfbae5bf6b37485fa3d
tutulits <arthur07apolonio@gmail.com>
2024-12-03 19:47:24 -0300

    erros

---
+++ src/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Bem-vindo ao Dashboard!</h2>
+            <p>Você está logado e pode acessar todas as funcionalidades protegidas.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
